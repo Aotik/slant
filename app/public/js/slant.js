60d41157eeaa84e2e708113ac748c9e0020bfc49
Stefan Mansson <st.mansson@icloud.com>
2016-11-15 20:12:59 +0000

    Search results
    Search results UI, torrent links and hidden poster if N/A

--- app/public/js/slant.js
+++ app/public/js/slant.js
@@ -10,12 +10,36 @@ $(document).ready(() => {
 		$(e.target).addClass('selected')
 	})
 
+	$('body').on('click', '.js_res', (e) => {
+		data = {}
+
+		if(e.target.tagName.toLowerCase() === 'a') {
+			data.id = $(e.target).data('imdb')
+		} else {
+			data.id = $(e.target).closest('.js_res').data('imdb')
+		}
+
+		data.url = "/api/v1/imdbid"
+
+		$.ajax({
+			url: data.url,
+			type: "POST",
+			contentType: 'application/json',
+			data: JSON.stringify(data),
+			success: (data) => {
+				$('.search').slideUp(120, () => {
+					transitionResult(data)
+				})
+			}
+		})
+	})
+
 	$('.go').bind('click', (e) => {
 		data = {}
 
 		if (o.selected == "title") {
 			data.title = $('.content').val().trim()
-			data.url = "/api/v1/title"
+			data.url = "/api/v1/title/search"
 		} else {
 			data.id = $('.content').val().trim()
 			data.url = "/api/v1/imdbid"
@@ -26,30 +50,75 @@ $(document).ready(() => {
 			type: "POST",
 			contentType: 'application/json',
 			data: JSON.stringify(data),
-			success: (data) => {
-				$('.search').slideUp(120, () => {
-					transitionResults(data)
-				})
+			success: (results) => {
+				transitionResults(results)
 			}
 		})
 
 	})
 
 	transitionSearch = () => {
-		$('#results').empty()
+		$('#result .within').empty()
 		$('body').removeClass('light dark')
 		$('body').css('background-color', '#fff')
-		$('#results').slideUp(120)
+		$('#result').slideUp(120)
 		$('.search').slideDown(140)
 	}
 
-	transitionResults = (res) => {
-		res.poster.replace('SX300', 'SX6000')
-		$('#results').append(`
+	transitionResults = (results) => {
+		$('#result, .search, #results').hide()
+		$('#results .within').empty()
+		$('body').removeClass('light dark')
+		$('body').css('background-color', '#fff')
+
+		$('#results .within').append('<div class="class="xs-12"><h3>Search results</h3></div>')
+		results = JSON.parse(results)
+		console.log(results);
+		if (results.Response != 'False') {
+			$.each(results.Search, (a,b) => {
+				$('#results .within').append(`
+					<a data-imdb="${b.imdbID}" class="xs-12 js_res">
+						${b.Title} <span>(${b.Year})</span>
+						<i class="material-icons">chevron_right</i>
+					</a>
+				`)
+			})
+
+			$('#results').slideDown(140)
+		} else {
+			console.log('no results')
+		}
+	}
+
+	transitionResult = (res) => {
+		$('#results').slideUp(140)
+
+		$.get(`https://yts.ag/api/v2/list_movies.json?sort=seeds&query_term=${res.imdbid}`, (da) => {
+			da.data.movie_count == 0 ? torrURL = null : torrURL = da.data.movies[0].torrents[0].url
+			appendResult(res, torrURL)
+		})
+
+	}
+
+	appendResult = (res, torrURL) => {
+		$('#result').hide()
+
+		if (res.poster == "N/A") {
+			res.poster = null
+		} else {
+			res.poster.replace('SX300', 'SX1000')
+		}
+
+		torrURL != null ? hasMagnet = `<a class="button green torrent" href="${torrURL}" target="_blank"><i class="material-icons">file_download</i> Download Torrent</a>` : hasMagnet = ''
+
+		res.poster != null ? hasPoster = `<div class="poster" style="background-image: url(${res.poster})" data-adaptive-background data-ab-css-background></div>` : hasPoster = ''
+
+		$('#result .within').append(`
 			<div class="grid-row">
 				<div class="side xs-12 s-4">
-					<div class="poster" style="background-image: url(${res.poster.replace('SX300', 'SX1000')})" data-adaptive-background data-ab-css-background></div>
+					${hasPoster}
 					<a class="button black imdbbtn" href="${res.imdburl}" target="_blank">View on IMDB</a>
+					${hasMagnet}
 					<a class="button primary reset" onclick="transitionSearch()"><i class="material-icons">arrow_back</i> Search for another movie</a>
 				</div>
 				<div class="info xs-12 s-8">
@@ -87,15 +156,17 @@ $(document).ready(() => {
 			</div>
 		`)
 
-		$('#results').hide()
 
-		$.adaptiveBackground.run({
-			success: ($img, data) => {
-				$('body').css('background-color', data.color)
-				$('body').lightOrDark()
-				$('#results').slideDown(140)
-			}
-		})
-
+		if (res.poster != null) {
+			$.adaptiveBackground.run({
+				success: ($img, data) => {
+					$('body').css('background-color', data.color)
+					$('body').lightOrDark()
+					$('#result').slideDown(140)
+				}
+			})
+		} else {
+			$('#result').slideDown(140)
+		}
 	}
 })
--- app/routes/endpoint.js
+++ app/routes/endpoint.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const router = express.Router()
 
+const request = require('request')
 const imdb = require('imdb-api')
 
 router.post('/title', (req, res) => {
 	imdb.get(req.body.title).then((data) => {
-		console.log(data)
 		return res.send(data)
 	})
+})
 
+router.post('/imdbid', (req, res) => {
+	console.log(req.body.id);
+	imdb.getById(req.body.id).then((data) => {
+		return res.send(data)
+	})
+})
+
+router.post('/title/search', (req, res) => {
+	request('http://www.omdbapi.com/?s=' + req.body.title, (error, response, body) => {
+		if (!error && response.statusCode == 200) {
+			return res.send(body)
+		}
+	})
 })
 
 module.exports = router
